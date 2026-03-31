460630df9a94b697d6e24e7ff3f1b1dadafa89ba
alanabenitez1804-png <alanabenitez1804@gmail.com>
2026-03-31 19:32:38 -0300

    Revert "delete main.py"
    This reverts commit 7c39367f8a54b1740ad7376b706d9598f1b467cb.

---
+++ main.py
@@ -0,0 +1,11 @@
+print("hola wacho")
+
+palabra=input("ingrese su nombre : ")
+
+print(f"HOLA {palabra} ! BIENVENIDO A LA PROGRAMACIÓN")
+
+edad=int(input("ingrese su edad: "))
+
+anio=int(input("ingrese año actual: "))
+
+print(f"Su fecha de nacimiento es: {anio-edad}")
