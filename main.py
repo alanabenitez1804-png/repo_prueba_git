58a095dceb75eb5e087c620af2c421c24c60fe91
alanabenitez1804-png <alanabenitez1804@gmail.com>
2026-03-31 18:56:46 -0300

    Usando Ramas
    Se agrego mini programa para saber año de nacimiento

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("hola wacho")
 palabra=input("ingrese su nombre : ")
 
 print(f"HOLA {palabra} ! BIENVENIDO A LA PROGRAMACIÓN")
+
+edad=int(input("ingrese su edad: "))
+
+anio=int(input("ingrese año actual: "))
+
+print(f"Su fecha de nacimiento es: {anio-edad}")
